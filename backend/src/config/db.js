199b76f2e745b6364ef91340478e9761b10e363a
dngsondev <dngsondev@gmail.com>
2025-07-20 00:41:07 +0700

    loi mat ket noi mysql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -29,7 +29,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
