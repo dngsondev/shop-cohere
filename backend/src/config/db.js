f9fc945210c11ec9eb15cb47b2b1cad38d9378c5
dngsondev <dngsondev@gmail.com>
2025-07-20 00:36:46 +0700

    loi mat ket noi mysql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,3 +1,29 @@
+// import mysql from 'mysql2';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     // Thêm cấu hình tối ưu cho xử lý nhiều files
+//     //   acquireTimeout: 60000,
+//     //   timeout: 60000,
+//     //   reconnect: true,
+//     //   maxReconnects: 3
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('❌ Error connecting to the database:', err);
+//     } else {
+//         console.log('✅ Connected to the MySQL database!');
+//     }
+// });
+
+// export default connection;
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
@@ -8,19 +34,9 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    // Thêm cấu hình tối ưu cho xử lý nhiều files
-    //   acquireTimeout: 60000,
-    //   timeout: 60000,
-    //   reconnect: true,
-    //   maxReconnects: 3
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('❌ Error connecting to the database:', err);
-    } else {
-        console.log('✅ Connected to the MySQL database!');
-    }
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 export default connection;
