1fc469588fbcd673e495454da6162d961bb561ab
dngsondev <dngsondev@gmail.com>
2025-08-22 12:56:13 +0700

    final

--- backend/src/config/vnpayConfig.js
+++ backend/src/config/vnpayConfig.js
@@ -8,9 +8,16 @@ dotenv.config();
 //     vnp_ReturnUrl: 'http://localhost:5173/order/vnpay-result',
 // };
 
+function joinUrl(base, path) {
+    if (base.endsWith('/')) base = base.slice(0, -1);
+    if (!path.startsWith('/')) path = '/' + path;
+    return base + path;
+}
+
 export default {
     vnp_TmnCode: process.env.VNP_TMNCODE,
     vnp_HashSecret: process.env.VNP_HASHSECRET,
     vnp_Url: process.env.VNP_URL,
-    vnp_ReturnUrl: process.env.FRONTEND_URL + 'order/vnpay-result',
+    //vnp_ReturnUrl: process.env.FRONTEND_URL + 'order/vnpay-result',
+    vnp_ReturnUrl: joinUrl(process.env.FRONTEND_URL, '/order/vnpay-result'),
 };
